3744fa94cb77e6061da60fbcdf6204a26bc7454b
Chauncey Tanner Murphey <47878298+tanner-murphey@users.noreply.github.com>
2020-05-03 16:27:19 -0700

    Includes correct modules

--- Pedal_Board.sv
+++ Pedal_Board.sv
@@ -3,62 +3,74 @@ module Pedal_Board(input logic [15:0] Signal_in,
 							input logic [17:0] Switches,
 							output logic [15:0] Signal_out);
 							
-logic od_start
+logic od_start, trem_start, vib_start, echo_start;
 
-//clean clean_pedal(.in(Signal_in),
-//						.out(clean_out));
-//
+
 overdrive_effect OD_pedal(	.input_frame(Signal_in),
 									.CLK(Clk),
 									.START(od_start),
-									.Done(OD_done),
-									.gain(Switches[0]),
-									.output_frame(dist_out));
+									.DONE(OD_done),
+									.gain(Switches[16]),
+									.output_frame(OD_out));
 									
-// Hellooooooo
 
-//mux4_1 distmux(.d0(Signal_in),
-//					.d1(clean_out)
-//					.d2(dist_out),
-//					.d3(Signal_in),
-//					.s(Switches[17:16]),
-//					.out(distmux_out));
-//
-//compressor comp_pedal(	.in(distmux_out),
-//								.out(comp_out));
-//								
-//mux2_1 compmux(.d0(distmux_out),
-//					.d1(comp_out),
-//					.s(Switches[15]),
-//					.out(compmux_out));
-//					
-//looper loop_pedal(.in(compmux_out),
-//						.out(looper_out));
-//				
-//mux2_1 loopmux(.d0(compmux_out),
-//					.d1(looper_out)
-//					.s(Switches[14]),
-//					.out(loopmux_out));
-//					
-//reverb reverb_pedal(	.in(loopmux_out),
-//							.out(revb_out));
-//
-//mux2_1 revbmux(.d0(loopmux_out),
-//					.d1(revb_out),
-//					.s(Switches[13]),
-//					.out(revbmux_out));
+
+mux2_1 distmux(.d0(Signal_in),
+					.d1(OD_out),
+					.s(Switches[17]),
+					.out(distmux_out));
+
+tremolo trem_pedal(	.in(distmux_out),
+								.Clk(Clk),
+								.START(trem_start),
+								.DONE(trem_done),
+								.speed(Switches[13]),
+								.out(trem_out));
+								
+mux2_1 tremmux(.d0(distmux_out),
+					.d1(trem_out),
+					.s(Switches[14]),
+					.out(tremmux_out));
+					
+vibrato vib_pedal(	.in(compmux_out),
+							.START(vib_start),
+							.DONE(vib_done),
+							.speed(Switches[10])
+							.out(vib_out));
+				
+mux2_1 vibmux(	.d0(tremmux_out),
+						.d1(vib_out)
+						.s(Switches[11]),
+						.out(vibmux_out));
+					
+echo echo_pedal(.in(vibmux_out),
+						.START(echo_start),
+						.DONE(echo_done),
+						.time(Switches[7]),
+						.out(echo_out));
+
+mux2_1 echomux(	.d0(vibmuxmux_out),
+						.d1(echo_out),
+						.s(Switches[8]),
+						.out(echomux_out));
 
 always_comb
 	begin				
-	//Signal_out = revbmux_out;
-	Signal_out = Signal_in;
+	Signal_out = echomux_out;
+	//Signal_out = Signal_in;
 	end
 always_ff @(posedge Clk)
 	begin
-		if(OD_done)
+		if(OD_done & trem_done & vib_done & echo_done)
 			od_start <= 0;
+			trem_start <= 0;
+			vib_start <= 0;
+			echo_start <= 0;
 		else
 			od_start <= 1;
+			trem_start <= 1;
+			vib_start <= 1;
+			echo_start <= 1;
 
 
 	end
